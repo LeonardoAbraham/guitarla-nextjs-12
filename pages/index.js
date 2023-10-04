afa976099868647c60bc98a1b71f8bc8e9d1a14d
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-04 16:43:36 -0600

    El modelo basado en Rutas y Link en Next

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-
+import Link from "next/link"
 
 export default function Home() {
     return (
-        <h1>
-            Hola mundo en next
-        </h1>
+        <>
+            <h1>
+                Hola mundo en next
+            </h1>
+
+            <Link href={"/nosotros"}>Nosotros</Link>
+        </>
+        
     )
 }
---
+++ pages/nosotros.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const Nosotros = () => {
+    return (
+        <div>
+            <Link href={"/"}>Ir a inicio</Link>
+        </div>
+
+    )
+}
+
+export default Nosotros

303b8aabbd89539cd9c846591f26e6d81bbcd63b
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-04 16:54:00 -0600

    Creando Layout principal

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Link from "next/link"
+import Layout from "../components/layout"
 
 export default function Home() {
     return (
         <>
-            <h1>
-                Hola mundo en next
-            </h1>
+            <Layout>
+                <h1>
+                    Hola mundo en next
+                </h1>
 
-            <Link href={"/nosotros"}>Nosotros</Link>
+                <Link href={"/nosotros"}>Nosotros</Link>
+
+            </Layout>
+            
         </>
         
     )
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import Layout from "../components/layout"
 
 const Nosotros = () => {
     return (
-        <div>
+        <Layout>
             <Link href={"/"}>Ir a inicio</Link>
-        </div>
+        </Layout>
 
     )
 }

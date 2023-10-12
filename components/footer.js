6b83265cb01ffb9c0c8ee39ac7235c49432d290f
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-12 11:27:03 -0600

    Creando el Footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,30 @@
-
+import Link from "next/link"
+const styles = {}
 
 const Footer = () => {
     return (
-        <div>footer</div>
+        <footer>
+            <div className="contenedor">
+                <nav className={styles.navegacion}>
+                    <Link href={"/"}>
+                        Inicio
+                    </Link>
+
+                    <Link href={"/nosotros"}>
+                        Nosotros
+                    </Link>
+
+                    <Link href={"/blog"}>
+                        Blog
+                    </Link>
+
+                    <Link href={"/tienda"}>
+                        Tienda
+                    </Link>
+                </nav>
+                <p>Todos los derechos reservados {new Date().getFullYear()}</p>
+            </div>
+        </footer>
     )
 }
 

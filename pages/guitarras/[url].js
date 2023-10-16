423de712cf281a2a2eb4a95fe2c4e7eabaa4baef
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-16 12:24:27 -0600

    Routing dinamico con getStaticPaths y getStaticProps

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,13 +1,28 @@
 
 const Producto = ({guitarra}) => {
     console.log(guitarra[0].attributes.nombre)
-    
+
     return (
         <div>Producto</div>
     )
 }
 
-export async function getServerSideProps({query:{url}}){
+export async function getStaticPaths(){
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras`)
+    const {data} = await respuesta.json()
+
+    const paths = data.map(guitarra => ({
+        params: {
+            url:guitarra.attributes.url
+        }
+    }))
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params:{url}}){
 
     const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
     const {data:guitarra} = await respuesta.json()
@@ -18,5 +33,18 @@ export async function getServerSideProps({query:{url}}){
         }
     }
 }
+
+
+//export async function getServerSideProps({query:{url}}){
+//
+//    const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
+//    const {data:guitarra} = await respuesta.json()
+//
+//    return {
+//        props: {
+//            guitarra
+//        }
+//    }
+//}
 
 export default Producto
